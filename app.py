d7a6568fd916c47370f5335c4839d5a84cca9d37
João Gabriel Soares <jgsoares.francisco@gmail.com>
2023-01-24 01:34:18 -0300

    Update func

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_cors import CORS
 from src import Calculo
 app = Flask(__name__)
 CORS(app)
 
+app.template_folder = './frontend'
+
+
 CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:5500"}})
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
 @app.route('/calcula', methods=['POST'])
 def calcula():
@@ -16,11 +19,17 @@ def calcula():
     cateto1 = request.json.get('cateto1')
     cateto2 = request.json.get('cateto2')
 
+    print(hipotenusa)
+    print(cateto1)
+    print(cateto2)
+
     calculo = Calculo.Calculo(
         hipotenusa = hipotenusa,
         cateto1 = cateto1,
         cateto2 = cateto2
         )
+    
+    print(calculo)
     
     return calculo.pegar_retorno()
 
