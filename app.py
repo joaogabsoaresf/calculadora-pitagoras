76542d54fba8ab9cf54ac9992afe55c77f05b6af
João Gabriel Soares <jgsoares.francisco@gmail.com>
2023-01-25 23:53:46 -0300

    Doc

--- app.py
+++ app.py
@@ -6,12 +6,21 @@ CORS(app)
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
+
 @app.route('/', methods=['GET'])
 def index():
+    """
+    Rota principal da aplicação. Retorna a string 'Hello World' quando acessada via método GET.
+    """
     return 'Hello World'
 
 @app.route('/calcula', methods=['POST'])
 def calcula():
+    """
+    Esta função é chamada quando a rota '/calcula' é acessada via método POST.
+    Recebe os valores de hipotenusa, cateto1 e cateto2 via requisição JSON e realiza um cálculo
+    utilizando a classe Calculo. Em seguida, retorna o resultado do cálculo.
+    """
 
     print(request.json)
 
--- src/Calculo.py
+++ src/Calculo.py
@@ -1,10 +1,19 @@
 class Calculo:
     def __init__(self, **kwargs) -> None:
+        """
+        Inicializa os atributos hipotenusa, cateto1 e cateto2 com os valores passados como argumentos.
+        Os valores são obtidos a partir dos argumentos passados como keyword (kwargs)
+        """
         self.hipotenusa = kwargs.get('hipotenusa')
         self.cateto1 = kwargs.get('cateto1')
         self.cateto2 = kwargs.get('cateto2')
 
     def pegar_retorno(self):
+        """
+        Verifica qual dos atributos não foi passado como argumento e calcula o valor
+        correspondente. Retorna um dicionário com a chave 'side' indicando qual lado foi calculado e 
+        a chave 'result' com o valor calculado arredondado para 4 casas decimais.
+        """
         if not self.hipotenusa:
             return {'side':'hipotenusa', 'result': round((self.cateto1 ** 2 + self.cateto2 ** 2) ** (1/2), 4)}
         
